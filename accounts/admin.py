999198d3e2a4f0f2ade388650ef06c6012ed41a5
fauziahpf <sissyfauziah@gmail.com>
2022-11-01 05:47:33 +0700

    Edit admin fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -17,18 +17,21 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('name', 'email','username','is_admin','is_staff', 'is_regular', 'is_bank','weight','score','id',)
     search_fields = ('email','username', 'name')
     readonly_fields = ('id',)
-    add_fieldsets = (
-        (None, {
-            'fields': ('name', 'email', 'password1', 'password2', 'is_superuser', 'is_staff', 'is_admin', 'gender', 'city', 'address')}
-        ),
-    )
+    
     fieldsets = (
     (None, {'fields': ('name', 'password', 'username', 'is_regular', 'is_bank', 'gender', 'city', 'address', 'weight', 'score')}),
     (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
     (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
     (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     (_('Groups'), {'fields': ('groups',)}),
-)
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'fields': ('name', 'email', 'password1', 'password2', 'is_superuser', 'is_staff', 'is_admin', 'gender', 'city', 'address')}
+        ),
+    )
+    
     # fieldsets = (
     #     (None, {
     #         'fields': ('name', 'city', 'email','username','is_admin','is_staff', 'is_regular', 'is_bank', 'gender')
